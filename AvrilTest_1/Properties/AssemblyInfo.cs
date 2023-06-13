393887b06ab94f2b7c61c4ccf20c8bd425de7cae
claysona <claysona@9HS7MQ2>
2023-06-13 14:23:05 +0100

    Add project files.

---
+++ AvrilTest_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AvrilTest_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("AvrilTest_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("C0851522-2245-4192-AC51-86D5AB145C73")]
+[assembly: AssemblyVersion("1.0.0.0")]
